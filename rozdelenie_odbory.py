70b37fa1847b636054e2199d0b81e254960ac5ce
Your Name <you@example.com>
2024-12-09 08:56:30 +0100

    vytvorenie funkcie pocet_ziakov_podla_odboru

--- rozdelenie_odbory.py
+++ rozdelenie_odbory.py
@@ -7,5 +7,19 @@ def nacitaj_subor(nazov):
 def pocet_ziakov_spolu(zoznam):
     return len(zoznam)
 
+def pocet_ziakov_podla_odboru(zoznam, odbor):
+    pocet = 0
+    for ziak in zoznam:
+        rozdeleny = ziak.split(',') # ['Radovan Kliescik', 'IST\n']
+        meno = rozdeleny[0]
+        odbor_ziaka = rozdeleny[1].strip()
+        if odbor_ziaka == odbor:
+            pocet += 1
+    return pocet
+
+
+
+
 nacitany_subor = nacitaj_subor('odbory.txt')
 #print(pocet_ziakov_spolu(nacitany_subor))
+print(pocet_ziakov_podla_odboru(nacitany_subor, 'IST'))
