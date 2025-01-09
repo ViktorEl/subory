e819b781f097fd95f45f47af181d849c2e6f835e
Your Name <you@example.com>
2025-01-09 10:42:40 +0100

    pridanie funkcie neprospievajucich

--- prospech.py
+++ prospech.py
@@ -1,5 +1,4 @@
 
-
 def nacitanie_suboru(nazov):
     with open(nazov, 'r', encoding='utf-8') as f:
         nacitany_subor = f.readlines()
@@ -21,7 +20,25 @@ def znamka_podla_mena(meno, predmet, zoznam):
         return 'meno alebo predmet neexistuje'
 
 
+'''Funkcia vráti zoznam neprospievajucich ziakov z daného predmetu'''
+def neprospievajuci(predmet, zoznam):
+    novy_zoznam = []
+    for prvok in zoznam:
+        rozdelenie = prvok.split(',') 
+        meno_ziaka = rozdelenie[0].strip()
+        predmet_ziaka = rozdelenie[1].strip()
+        znamka = rozdelenie[2]
+        znamka = int(znamka)
+        if znamka == 5 and predmet == predmet_ziaka:
+            novy_zoznam.append(meno_ziaka)
+    if len(novy_zoznam) == 0:
+        return '0 neprospievajucich ziakov'
+    else:
+        return novy_zoznam
+
+
 
   
 ziaci = nacitanie_suboru('subor.txt')
-#print(znamka_podla_mena('Milan', 'SIE', ziaci))
+# print(znamka_podla_mena('Milan', 'SIE', ziaci))
+print(neprospievajuci('SJL', ziaci))
